f725f2cc96cd307bfc577b75263d7049455855cc
Ethan Phillippi <ethan@stackasaur.us>
2024-03-13 12:25:42 -0400

    comments and gc

--- src/lib/html.ts
+++ src/lib/html.ts
@@ -1,44 +1,69 @@
 import { getId, isEvent } from "./util";
 import {isSignal} from "./signal";
 
-function isDynamic(obj: unknown){
+/**
+ * isDynamic
+ * @description determines if an Object is subject to change or not
+ * @param {unknown}obj
+ * @returns {boolean}
+ */
+function isDynamic(obj: unknown): obj is object|Function|Node|typeof isSignal{
     return typeof obj === "function" || obj instanceof Node || isSignal(obj);
 }
 
-/** based on the previous template strings, determine if the dynamic
- *  expression is used as an attribute
+/** 
+ * getAttribute
+ * @description based on the template string, determines if a variable is used
+ * in the context of an attribute. Returns the attribute name if so, otherwise
+ * returns undefined
+ * @param {string}prev the previous template string
+ * @returns {string|undefined}
 **/
 function getAttribute(prev:string){
     for (let i=prev.length-1;i>=0;i--){
         const c = prev.charAt(i);
 
+        // if a close angle is the notable first character seen, it must be
+        // in a tag
         if (c === ">")
             return undefined;
 
+        // currently, when used in an attribute, quotes aren't supported.
+        // if it's an equals sign, look backwards for the attribute name
         else if (c === "="){
             let attNameEnd;
             let attNameStart;
             for (let j=i-1;j>=0;j--){
                 const c = prev.charAt(j);
+
+                // if the attribute name end exists and a space is reached, the
+                // the name has been passed.
                 if (attNameEnd != null && c === " "){
                     attNameStart = j+1;
                     break;
-                } else if (attNameEnd == null){
+                } 
+                // if the name end hasn't been set yet and the current
+                // character isn't a space, it's the end.
+                else if (attNameEnd == null && c !== " "){
                     attNameEnd = j+1;
                 }
             }
             if (attNameStart != null && attNameEnd != null){
                 return prev.substring(attNameStart,attNameEnd);
             }
-        } else if (c !== " "){
+        } 
+        // if anything else, it can't be an attribute
+        // TODO: support quotes?
+        else if (c !== " "){
             return undefined;
         }
     }
     return undefined;
 }
+// events must have on:event syntax to pass the built in parser
 const eventRegex = /on:[a-z]+/;
 function html(templateStrings:TemplateStringsArray, ...args: Array<unknown>): DocumentFragment{
-    // const strings = [...templateStrings];
+    // use a template because anything can be a child of a template
     const wrapper = document.createElement("template");
     const chunks: Array<String> = [];
 
@@ -47,7 +72,7 @@ function html(templateStrings:TemplateStringsArray, ...args: Array<unknown>): Do
     const keyMap = new Map();
     const events: Set<keyof ElementEventMap> = new Set();
 
-    if (args != null){        
+    if (args != null){   
         args.forEach((arg,idx)=>{
             let prev = templateStrings[idx];
             // let next = templateStrings.length>idx+1
@@ -55,47 +80,48 @@ function html(templateStrings:TemplateStringsArray, ...args: Array<unknown>): Do
             //     : null;            
             
             if (isDynamic(arg)){
-                const id = keyMap.has(arg)
-                    ? keyMap.get(arg)
-                    : (getId());
-                
+                // get a locally unique identifier to use to find elements
+                const id = getId();
                 resourceMap.set(id,arg);
                 keyMap.set(arg,id);
 
                 const attName = getAttribute(prev);
-                // tag
+                // content, insert a placeholder element
                 if (attName == null){
                     chunks.push(prev,`<ins id="${id}"></ins>`);
                 }
-                // listener
+                // listener, store the key for the resource as the on:event val
                 else if (eventRegex.test(attName) && isEvent(attName.substring(3))){
-                    //@ts-ignore
+                    //@ts-ignore - TODO: fix this one
                     const event: keyof ElementEventMap = attName.substring(3);
-
                     if (!events.has(event)){
                         events.add(event);
                     }
-
                     chunks.push(prev,`"${id}"`);
                 }
-                // att
+                // att, set the attribute as [att="id"] for querying,
+                // map the key to the attribute name for reference
                 else {
                     attributeMap.set(id,attName);
                     chunks.push(prev,`"${id}"`);
                 }
-            } else {
+            } 
+            // if the value isn't a tracked dynamic type, just cast it
+            else {
                 chunks.push(prev,String(arg));
             }
         });
     }
     chunks.push(templateStrings[templateStrings.length-1]);
 
-    // try to use experimental
-    if ("setHTML" in wrapper && typeof wrapper.setHTML === "function")
-        wrapper.setHTML(chunks.join(""));
-    else
+    // TODO: try to use experimental setHTML
+    // if ("setHTML" in wrapper && typeof wrapper.setHTML === "function")
+    //     wrapper.setHTML(chunks.join(""));
+    // else
         wrapper.innerHTML = chunks.join("");
 
+    // for each tracked event, find all referencing elements, create proper
+    // event listener and remove attribute
     for(const event of events){
         const els = wrapper.content.querySelectorAll(`[on\\:${event}]`);
         for (const el of els){
@@ -104,9 +130,33 @@ function html(templateStrings:TemplateStringsArray, ...args: Array<unknown>): Do
                 const func = resourceMap.get(att);
                 el.addEventListener(event,func);
                 el.removeAttribute(`on:${event}`);
+                resourceMap.delete(att);
             }
         }
     }
+    // for everything in the attribute map, find the element, update the
+    // attribute
+    attributeMap.forEach((value,key)=>{
+        let el = wrapper.content.querySelector(`[${value}="${key}"]`);
+        if (el != null && resourceMap.has(key)){
+            const resource = resourceMap.get(key);
+            if (isSignal(resource)){
+                resource.subscribe((i:unknown)=>{
+                    if (el != null){
+                        el.setAttribute(value,String(i));
+                    }
+                });
+            } else if (typeof resource === "function"){
+                console.log("TODO FUNCTION")
+            } else{
+                el.removeAttribute(value);
+            }
+            resourceMap.delete(key);
+        }
+    });
+
+    // for everything left in the resourcemap, find the element, and insert
+    // the resource
     resourceMap.forEach((value,key)=>{
         let el:Element|Text|null = wrapper.content.querySelector(`#${key}`);
         if (el != null){
@@ -130,23 +180,14 @@ function html(templateStrings:TemplateStringsArray, ...args: Array<unknown>): Do
         }
     });
 
-    attributeMap.forEach((value,key)=>{
-        let el = wrapper.content.querySelector(`[${value}="${key}"]`);
-        if (el != null && resourceMap.has(key)){
-            const resource = resourceMap.get(key);
-            if (isSignal(resource)){
-                resource.subscribe((i:unknown)=>{
-                    if (el != null){
-                        el.setAttribute(value,String(i));
-                    }
-                });
-            } else if (typeof resource === "function"){
-                console.log("TODO FUNCTION")
-            } else{
-                el.removeAttribute(value);
-            }
-        }
-    });
+    
+
+    // clear all the maps/sets/arrays
+    resourceMap.clear();
+    keyMap.clear();
+    attributeMap.clear();
+    events.clear();
+    chunks.length = 0;
 
     return wrapper.content;
 }
