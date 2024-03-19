eb8e543ff1e3b94ad38ff4141b9612f1d0a4d93a
Ethan Phillippi <ethan@stackasaur.us>
2024-03-19 11:42:57 -0400

    Docs (#7)
    * add linter
    
    * use tw for pages
    
    * fix ts errors
    
    * clean up
    
    * out of place
    
    ---------
    
    Signed-off-by: Ethan Phillippi <ethan@stackasaur.us>

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,21 +1,27 @@
+type subscriptionFn = (v:unknown)=>void;
+type updateFn = (v:unknown)=>unknown;
+
+const noop = ()=>{};
+
 function Reactive(val:unknown){
-    const subscriptions: Set<Function> = new Set();
+    const subscriptions: Set<subscriptionFn> = new Set();
     let value = val;
     
     function get(){
-        let val;
-        subscribe((v:unknown)=>{val=v})();
-        return val;
+        return value;
+        // let val;
+        // subscribe((v:unknown)=>{val=v})();
+        // return val;
     }
     function set(val:unknown){
       value = val;
       fire();
     }
-    function update(fn:Function=()=>{}){
+    function update(fn:updateFn=noop){
         set(fn(value));
     }
   
-    function subscribe(run:Function=()=>{}){
+    function subscribe(run:subscriptionFn=noop){
       subscriptions.add(run);
       run(value);
       return ()=>{subscriptions.delete(run)};
@@ -23,18 +29,41 @@ function Reactive(val:unknown){
     function fire(){
         subscriptions.forEach(i=>i(value));
     }
+    function clear(){
+        subscriptions.clear();
+    }
 
     return {
         subscribe,
         update,
-        set,
         get,
-        fire
+        set,
+        get value(){
+            return get();
+        },
+        set value(v){
+            set(v);
+        },
+        fire,
+        clear
     }
+}
+
+function Readable(val:unknown){
+    const reactive = Reactive(val);
+    return {
+        subscribe:reactive.subscribe,
+        get:reactive.get,
+        get value(){
+            return reactive.get();
+        },
+        clear:reactive.clear,
+        fire:reactive.fire
+    } 
 }
 
 function isReactive(obj: unknown): obj is ReturnType<typeof Reactive>{
     return (obj != null && typeof obj === "object" && "subscribe" in obj);
 }
 
-export {isReactive, Reactive, Reactive as default};
+export {isReactive, Reactive, Readable, Reactive as default};
